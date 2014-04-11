2f4c7c7eb199ddc61845972af3a0b7b73a16348d
Alexey Belkevich <belkevich.alexey@gmail.com>
2014-04-11 13:08:59 +0300

    Added empty test project

---
+++ Example/ReachabilityApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReachabilityApp
+//
+//  Created by Alexey Belkevich on 4/11/14.
+//  Copyright (c) 2014 okolodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ReachabilityApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReachabilityApp
+//
+//  Created by Alexey Belkevich on 4/11/14.
+//  Copyright (c) 2014 okolodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
